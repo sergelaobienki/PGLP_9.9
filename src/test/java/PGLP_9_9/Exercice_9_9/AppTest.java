46f7eb82f5751ca05f4ce331d5d711c9046fd7d7
KI Serge Laobien <sergelaobienk@gmail.com>
2020-05-16 22:33:27 +0000

    Creation des classe pour le dessin

---
+++ src/test/java/PGLP_9_9/Exercice_9_9/AppTest.java
@@ -0,0 +1,20 @@
+package PGLP_9_9.Exercice_9_9;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
