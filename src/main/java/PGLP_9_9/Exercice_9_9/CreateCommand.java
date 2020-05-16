3e2e20453b18fa4c994ca908311edeea2a2033ce
KI Serge Laobien <sergelaobienk@gmail.com>
2020-05-16 22:39:33 +0000

    Creation de la Basedonnee

---
+++ src/main/java/PGLP_9_9/Exercice_9_9/CreateCommand.java
@@ -0,0 +1,51 @@
+package PGLP_9_9.Exercice_9_9;
+
+public class CreateCommand implements Command {
+    /**
+     * Forme a créer.
+     */
+    private Forme form;
+    /**
+     * Constructeur.
+     * @param f La forme a créer
+     */
+    public CreateCommand(final Forme f) {
+        this.form = f;
+    }
+    /**
+     * Execute la commande de création.
+     */
+    public void execute() {
+        Connection c = DataBase.createBase();
+        FactoryDaoJDBC fdj = new FactoryDaoJDBC(c);
+        Forme f = null;
+        if (form.getClass() == Cercle.class) {
+            CercleDaoJDBC cercle = (CercleDaoJDBC)
+                    fdj.getCercleDao();
+            f = cercle.create((Cercle) form);
+        } else if (form.getClass() == Carre.class) {
+            CarreDaoJDBC carre = (CarreDaoJDBC) fdj.getCarreDao();
+            f = carre.create((Carre) form);
+        } else if (form.getClass() == Rectangle.class) {
+            RectangleDaoJDBC rectangle = (RectangleDaoJDBC)
+                    fdj.getRectangleDao();
+            f = rectangle.create((Rectangle) form);
+        } else if (form.getClass() == Triangle.class) {
+            TriangleDaoJDBC triangle = (TriangleDaoJDBC)
+                    fdj.getTriangleDao();
+            f = triangle.create((Triangle) form);
+        } else if (form.getClass() == Groupe.class) {
+            GroupeDaoJDBC groupe = (GroupeDaoJDBC)
+                    fdj.getGroupeDao();
+            f = groupe.create((Groupe) form);
+        }
+        if (f == null) {
+            System.err.println("Nom existe déjà");
+        }
+        try {
+            c.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
