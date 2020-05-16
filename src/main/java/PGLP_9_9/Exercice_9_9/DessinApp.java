8b49df5071c8f85d2bbe6f4066b62ed903fa42f7
KI Serge Laobien <sergelaobienk@gmail.com>
2020-05-16 22:44:30 +0000

    Creation de classe Dessin

---
+++ src/main/java/PGLP_9_9/Exercice_9_9/DessinApp.java
@@ -0,0 +1,63 @@
+package PGLP_9_9.Exercice_9_9;
+/**
+ * Classe DessinApp.
+ *
+ */
+public class DessinApp {
+	 /**
+     * Scanner.
+     */
+    private Scanner scanner;
+    /**
+     * DrawingTUI.
+     */
+    private DrawingTUI dt;
+    /**
+     * Constructeur.
+     */
+    public DessingApp() {
+        this.scanner = new Scanner(System.in);
+        dt = new DessinTUI();
+    }
+    /**
+     * Lance l'execution du programme.
+     * @param args
+     * @throws SQLException
+     */
+    public static void main(final String[] args) throws SQLException {
+        Connection connect = DataBase.createBase();
+        DataBase.deleteAllTables(connect);
+        DataBase.createAllTables(connect);
+        connect.close();
+        DessinApp da = new DessinApp();
+        da.run();
+    }
+    /**
+     * Execute le programme.
+     */
+    public void run() {
+        System.out.println("Exemples de commandes valides :\n"
+                + "Creation : \n"
+                + "c1 = Cercle((0, 0), 50)\n"
+                + "c2 = Carre((0, 0), 30)\n"
+                + "r1 = Rectangle((0, 0), 40, 10)\n"
+                + "t1 = Triangle((0, 0), (2, 2), (4, 0))\n"
+                + "g1 = Groupe(c1, c2, r1, t1)\n"
+                + "Déplacement : move(c1, (10, 20))\n"
+                + "Suppression : delete(r1, t1)\n"
+                + "Quitter : exit\n\n"
+                + "Entrer une commande :");
+        String command = scanner.nextLine();
+        Command c;
+        while (!command.equals("exit")) {
+            c = dt.nextCommand(command);
+            if (c != null) {
+                c.execute();
+                dt.drawAllCommand();
+            }
+            System.out.println("\nEntrer une nouvelle commande :");
+            command = scanner.nextLine();
+        }
+    }
+}
+}
